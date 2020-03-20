b1aaa73b14a9b270627256a5c74e9334f9922105
Mustafa Türk <mtafa.turk@gmail.com>
2020-03-20 23:14:33 +0100

    minor improvements

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 export default function Header() {
   return (
     <Wrapper>
-      <Links>
-        <Link href="https://github.com/mustafa-turk" target="_blank" rel="noopener">Github</Link>
-        <Link href="https://codepen.io/mustafa-turk" target="_blank" rel="noopener">Codepen</Link>
-      </Links>
+      <Navigation>
+        <Link href="https://github.com/mustafa-turk" target="_blank" rel="noopener">
+          Github
+          </Link>
+        <Link href="https://codepen.io/mustafa-turk" target="_blank" rel="noopener">
+          Codepen
+          </Link>
+        <Link href="/mustafaturk.pdf" target="_blank" rel="noopener">
+          Resume
+          </Link>
+      </Navigation>
       <h1>
-        👋 Hi, my name is Mustafa.
+        Hi, my name is Mustafa.
       </h1>
       <Description>
         I'm a software engineer based in Brussels, specializing in building (sometimes also designing) websites and applications.
@@ -21,15 +28,9 @@ const Wrapper = styled.div`
   margin-bottom: 30px;
   padding: 10px;
 `
-const Description = styled.p`
-  margin-top: 10px;
-  font-size: 23px;
-  @media (max-width: 500px) {
-    font-size: 19px;
-  }
-`
-const Links = styled.div`
-  margin-bottom: 15px;
+const Navigation = styled.div`
+  display: flex;
+  margin-bottom: 20px;
 `
 const Link = styled.a`
   display: inline-flex;
@@ -39,4 +40,11 @@ const Link = styled.a`
   &:hover {
     border-bottom: 1px solid black;
   }
-`;
+`
+const Description = styled.p`
+  margin-top: 10px;
+  font-size: 23px;
+  @media (max-width: 500px) {
+    font-size: 19px;
+  }
+`

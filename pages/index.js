f79929cae84e6ee933e1dbbab80d5c7a65f779ff
Mustafa Türk <mustafa.turk@skryv.com>
2020-04-12 21:24:47 +0200

    add google scripts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import styled, { createGlobalStyle } from 'styled-components'
-import withGA from 'next-ga'
 import Head from 'next/head'
-import Router from 'next/router'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ContentBlock from '../components/ContentBlock'
 
-function IndexPage() {
+export default function IndexPage() {
   return (
     <Wrapper>
       <GlobalStyle />
@@ -114,5 +112,3 @@ const Content = styled.div`
     display: block;
   }
 `
-
-export default withGA('UA-154421172-1', Router)(IndexPage)

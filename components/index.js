1c513fc0511a6c0a054174e2e6500e625d9e892d
Mustafa Türk <mustafa.turk@skryv.com>
2020-07-18 21:45:44 +0200

    refactor card and make component

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export * from './Layout';
 export * from "./Header";
 export * from "./Navbar";
+export * from "./Card";
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Header } from "../components"
+import { Layout, Header, Card } from "../components"
 import styled from "styled-components"
 
 export default function HomePage() {
@@ -7,60 +7,21 @@ export default function HomePage() {
     <Layout pageName='Hello world — Mustafa Türk'>
       <Header />
       <Content>
-        <ContentItem hasMarginRight>
-          <a
-            href='http://github.com/mustafa-turk/kopy'
-            target='_blank'
-            rel='noopener'
-          >
-            <img src='/work/1.jpg' alt='' />
-            <ContentOverlay>
-              <div>
-                <ContentOverlayTitle>kopy</ContentOverlayTitle>
-                <ContentOverlayBody>
-                  Clipboard application for macOS built with Electron.js and
-                  React.js
-                </ContentOverlayBody>
-              </div>
-            </ContentOverlay>
-          </a>
-        </ContentItem>
-        <ContentItem hasMarginRight>
-          <a
-            href='https://github.com/mustafa-turk/code-box'
-            target='_blank'
-            rel='noopener'
-          >
-            <img src='/work/2.jpg' alt='' />
-            <ContentOverlay>
-              <div>
-                <ContentOverlayTitle>code-box</ContentOverlayTitle>
-                <ContentOverlayBody>
-                  A web app where you can share code snippets with others
-                </ContentOverlayBody>
-              </div>
-            </ContentOverlay>
-          </a>
-        </ContentItem>
-        <ContentItem hasMarginRight>
-          <a
-            href='http://github.com/mustafa-turk/react-native-awesome-select'
-            target='_blank'
-            rel='noopener'
-          >
-            <img src='/work/3.jpg' alt='' />
-            <ContentOverlay>
-              <div>
-                <ContentOverlayTitle>
-                  react-native-awesome-select
-                </ContentOverlayTitle>
-                <ContentOverlayBody>
-                  A dead simple multiselect component for React Native
-                </ContentOverlayBody>
-              </div>
-            </ContentOverlay>
-          </a>
-        </ContentItem>
+        <Card
+          title="kopy"
+          description="Clipboard application for macOS built with Electron.js and React.js"
+          image="/work/1.jpg"
+          url="http://github.com/mustafa-turk/kopy" />
+        <Card
+          title="code-box"
+          description="A web app where you can share code snippets with others"
+          image="/work/2.jpg"
+          url="http://github.com/mustafa-turk/code-box" />
+        <Card
+          title="react-native-awesome-select"
+          description="A dead simple multiselect component for React Native"
+          image="/work/3.jpg"
+          url="http://github.com/mustafa-turk/react-native-awesome-select" />
       </Content>
     </Layout>
   );
@@ -73,46 +34,4 @@ const Content = styled.div`
   @media (max-width: 768px) {
     display: block;
   }
-`;
-
-const ContentItem = styled.div`
-  flex: 0 50%;
-  display: flex;
-  position: relative;
-  padding: 3px;
-  img {
-    width: 100%;
-  }
-`;
-
-const ContentOverlay = styled.div`
-  position: absolute;
-  width: calc(100% - 6px);
-  height: 100%;
-  bottom: 6px;
-  left: 10;
-  align-items: flex-end;
-  transition: .5s ease;
-  background: linear-gradient(to bottom, transparent 0%, rgba(0, 0, 0, 0.6) 81%);
-  display: flex;
-  opacity: 0;
-  border-radius: 5px;
-  &:hover {
-    opacity: 1;
-    cursor: pointer;
-  }
-  div {
-    color: white;
-    padding-left: 20px;
-    padding-bottom: 20px;
-  }
-`;
-
-const ContentOverlayTitle = styled.p`
-  font-weight: bold;
-  margin-bottom: 5px;
-`;
-
-const ContentOverlayBody = styled.p`
-  font-weight: normal;
 `;

3847cb6cbafc41a903d63e820303176026e2dfc8
Mustafa Türk <mustafa.turk@skryv.com>
2020-10-10 03:42:09 +0200

    update content

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,10 @@ export default function HomePage() {
       <Header />
       <Content>
         <Card
-          title="kopy"
-          description="Clipboard application for macOS built with Electron.js and React.js"
-          image="/work/1.jpg"
-          url="http://github.com/mustafa-turk/kopy" />
-        <Card
           title="kodebox"
           description="Web app where you can share code snippets with others"
           image="/work/2.jpg"
           url="http://github.com/mustafa-turk/kodebox" />
-        <Card
-          title="eksi-scraper"
-          description="Express app that will return a random entry from Eksi Sözlük"
-          image="/work/4.jpg"
-          url="http://github.com/mustafa-turk/eksi-scraper" />
         <Card
           title="react-native-awesome-select"
           description="Dead simple multiselect component for React Native"

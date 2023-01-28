35d75f13c140cb968b5629a3a759f4424196f106
Mustafa Turk <mtafa.turk@gmail.com>
2023-01-28 06:52:15 +0100

    remove google analytics scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Script from "next/script";
 
 import "styles/reset.css";
 import "styles/global.css";
@@ -13,19 +12,6 @@ export default function CustomApp({ Component, pageProps }) {
         <meta charSet='UTF-8' />
         <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
       </Head>
-      <Script
-        src='https://www.googletagmanager.com/gtag/js?id=UA-154421172-1'
-        strategy='afterInteractive'
-      />
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-154421172-1');
-        `}
-      </Script>
       <Component {...pageProps} />
     </>
   );

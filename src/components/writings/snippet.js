9b30306431cac76b745239ee633f8bc370462edf
Mustafa Turk <mtafa.turk@gmail.com>
2023-01-28 07:24:14 +0100

    fix transition effect

--- src/components/writings/snippet.js
+++
@@ -1,7 +0,0 @@
-export default function Snippet({ children }) {
-  return (
-    <div className="border-solid border border-neutral-800 p-3 overflow-scroll rounded-md">
-       {children}
-    </div>
-  )
-}

d3f8f71e58b94d0e8764fcebbab7cc2d8757d8b8
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2023-08-26 19:48:10 +0200

    add animation

--- src/components/home/layout.js
+++ src/components/home/layout.js
@@ -1,8 +1,17 @@
 import Header from "../common/header";
+import AnimatedBlob from "./animated-blob";
 
 export default function Layout({ children }) {
   return (
     <div className='flex flex-col gap-12 max-w-2xl p-5 mx-auto mt-6'>
+      <div className='absolute top-[-200px] left-[50%] w-[300px] mx-auto translate-x-[-100%]'>
+        <AnimatedBlob />
+      </div>
+
+      <div className='absolute top-[-200px] left-[50%] w-[300px] mx-auto translate-x-[-10%]'>
+        <AnimatedBlob />
+      </div>
+
       <Header />
 
       {children}

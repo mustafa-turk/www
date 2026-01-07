e467f7cc38d5c1f99e3dc7c1a198ec1b3b139ade
Mustafa Türk <43481284+mustafa-turk@users.noreply.github.com>
2026-01-07 18:08:58 +0100

    add redirects

---
+++ src/pages/calendar.js
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function Calendar() {
+  redirect('https://apps.apple.com/app/id6447790237')
+}
---
+++ src/pages/passgen.js
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function Passgen() {
+  redirect('https://secret.mustafaturk.me')
+}
